f52494041eece5ff14ea16b8372a2b2b336a5734
VibeDylan <dylanbourbotte62@icloud.com>
2026-04-21 12:57:34 +0200

    add login usecase and first controller

---
+++ src/auth/presentation/controllers/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Body, Post, Res } from "@nestjs/common";
+import { LoginUseCase } from "src/auth/application/use-cases/login.use-case";
+import { RegisterUseCase } from "src/auth/application/use-cases/register.use-case";
+import { RegisterDto } from "../dto/register.dto";
+import type { Response } from "express";
+
+@Controller('auth')
+export class AuthController {
+    constructor(
+        private readonly registerUseCase: RegisterUseCase,
+        private readonly loginUseCase: LoginUseCase
+    ) {}
+
+    @Post('register')
+    async register(@Body() data: RegisterDto, @Res() res: Response) {
+        const { user, accessToken, refreshToken } = await this.registerUseCase.execute(data);
+
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000, 
+        });
+
+        return res.json({
+            accessToken,
+            user: {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+            }
+        });
+    }
+}
