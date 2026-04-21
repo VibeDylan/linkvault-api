882790195ce09eec1e6b1bcd0a0e090b782ac9cb
VibeDylan <dylanbourbotte62@icloud.com>
2026-04-21 12:19:13 +0200

    add auth repository port

---
+++ src/auth/application/ports/auth.repository.port.ts
@@ -0,0 +1,18 @@
+import { UserEntity } from "src/auth/domain/user.entity";
+
+export interface IAuthRepository {
+    findByEmail(email: string): Promise<UserEntity | null>;
+    findById(id: string): Promise<UserEntity | null>;
+    createUser(data: {
+        email: string;
+        username: string;
+        passwordHash: string;
+    }): Promise<UserEntity>;
+    saveRefreshToken(data: {
+        token: string;
+        userId: string;
+        expiresAt: Date;
+    }): Promise<void>;
+    findRefreshToken(token: string): Promise<{userId: string, expiresAt: Date} | null>;
+    deleteRefreshToken(token: string): Promise<void>;
+}
