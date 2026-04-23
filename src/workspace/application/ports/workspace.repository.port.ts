aa3ad4b2287331c18b5bdaa87a277cbec728706b
VibeDylan <dylanbourbotte62@icloud.com>
2026-04-23 18:32:47 +0200

    add workspace repository

--- src/workspace/application/ports/workspace.repository.port.ts
+++ src/workspace/application/ports/workspace.repository.port.ts
@@ -1,7 +1,7 @@
 import { WorkspaceEntity } from "../../domain/workspace.entity";
 
 export interface IWOrkspaceRepository {
-    create(workspace: WorkspaceEntity): Promise<WorkspaceEntity>;
+    create(data: { name: string }): Promise<WorkspaceEntity>;
     findById(id: string): Promise<WorkspaceEntity | null>;
     listByUserId(userId: string): Promise<WorkspaceEntity[]>;
     inviteMember(workspaceId: string, userId: string, role: 'ADMIN' | 'MEMBER'): Promise<void>;
