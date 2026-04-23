a3fcb6523f86e2b703ef9a0869e85a4e54958595
VibeDylan <dylanbourbotte62@icloud.com>
2026-04-23 19:22:58 +0200

    wip

--- src/auth/application/use-cases/__tests__/register.use-case.spec.ts
+++ src/auth/application/use-cases/__tests__/register.use-case.spec.ts
@@ -1,5 +1,7 @@
 
+import { mock } from 'node:test';
 import { RegisterUseCase } from '../register.use-case';
+import { ConflictException } from '@nestjs/common';
 
 const mockRepository = {
     findByEmail: jest.fn(),
@@ -23,4 +25,44 @@ describe('RegisterUseCase', () => {
     })
 
     jest.clearAllMocks();
-});
+
+    it('should register a new user successfully', async () => {
+        mockRepository.findByEmail.mockResolvedValue(null);
+        mockRepository.createUser.mockResolvedValue({
+            id: 'user-id',
+            email: "test@example.com",
+            username: "testuser",
+            password: "hashedpassword",
+            createdAt: new Date()
+        })
+
+        const result = await useCase.execute({
+            email: "test@example.com",
+            username: "testuser",
+            password: "password123"
+        });
+
+
+        expect(result.user.email).toBe("test@example.com");
+        expect(result.user.username).toBe("testuser");
+        expect(mockRepository.createUser).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw ConflictException if email already exists', async () => {
+        mockRepository.findByEmail.mockResolvedValue({
+            id: 'uuid-123',
+            email: 'test@test.com',
+        });
+
+        // ACT + ASSERT
+        await expect(
+            useCase.execute({
+                email: 'test@test.com',
+                username: 'testuser',
+                password: 'password123',
+            }),
+        ).rejects.toThrow(ConflictException);
+
+        expect(mockRepository.createUser).not.toHaveBeenCalled();
+    });
+})
